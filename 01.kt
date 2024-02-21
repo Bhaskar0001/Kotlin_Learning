e6f0ef7a27b070e776f0b3d95bb709b7fdd8778b
Bhaskar0001 <96483290+Bhaskar0001@users.noreply.github.com>
2024-02-22 00:25:18 +0530

    Create 01.kt

---
+++ 01.kt
@@ -0,0 +1,25 @@
+fun main()
+{
+    println("hello")
+    //val cannot be change and var can be change 
+    var name = "ram"
+    val id = 1231
+    var city= "delhi"
+    
+    println(name)
+    println(id)
+    println("User name is" + name) //concatenation
+    println("User name is ${name}") //use of $ and string interpolation
+    
+    var a =45
+    var b = 89
+    println("sum of ${a} and ${b} is ${a+b}")
+    
+    a = 457
+    b =86
+    println("sum of ${a} and ${b} is ${a+b}")
+    
+   
+    
+}
+ 
