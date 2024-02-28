e3837f1b0be4efdc8a3f4291b7ec81567686aa20
Bhaskar001 <bhaskarjoshi900@gmail.com>
2024-02-29 02:16:28 +0530

    Operatr

--- Break,continue,return.kt
+++ Break,continue,return.kt
@@ -5,14 +5,34 @@ fun main() {
 //        println(i)
 //    if (i==5)
 //        break}
-    //labelled for loop
-      myloop@  for(i in 1..3) {
-            for (j in 1..3){
-                println("$i,$j")
-                if (i==2&&j==2)
-                    break@myloop
+//    //labelled for loop
+//      myloop@  for(i in 1..3) {
+//            for (j in 1..3){
+//                println("$i,$j")
+//                if (i==2&&j==2)
+//                    break@myloop
+//
+//        }        }
 
-        }        }
 
 
-    }
+    //continue Statement--to skip some value
+//    for (i in 1..9){
+//        if(i==5)
+//            continue
+//        println(i)
+//    }
+//    for (i in 1..9){
+//        if(i%2 == 0)
+//            continue
+//        println(i)
+//    }
+//labelled for loop--myloop or outer both
+//    outer@ for(i in 1..3) {
+//             for (j in 1..3){
+//                if (i==2&&j==2)
+//                   continue@outer
+//                println("$i,$j")
+//      }        }
+//
+   }
---
+++ method.kt
@@ -0,0 +1,32 @@
+fun main() {
+//    area(length = 34, breadth = 45)
+//    var larg_val = max(34,67)
+// println(larg_val)
+}
+
+//fun area(length:Int , breadth:Int){
+// print(length*breadth)
+//
+//}
+
+//if expression inside function
+//fun max(a:Int,b:Int):Int{
+//    if(b>a)
+//        return b
+//    else
+//        return a
+//}
+
+    //here in kotlin we can optimize code by converting function in expression as follows
+
+//    fun max(a:Int,b:Int):Int= if (a>b) {a} else {b}      //also as
+//fun max(a:Int,b:Int):Unit=
+//        if (a>b)
+//            println("$a is greater")
+//
+//
+//        else
+//            println("$b is greater")
+
+
+
