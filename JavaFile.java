9255e5b18a430cbbfa0ab2fda3443267c00338d8
Bhaskar001 <bhaskarjoshi900@gmail.com>
2024-02-29 23:16:59 +0530

    Operatr

---
+++ JavaFile.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.Array;
+
+public class JavaFile {
+    public static void main(String[] args){
+
+
+    }
+
+}
---
+++ interoperability.kt
@@ -0,0 +1,9 @@
+//we can call java functions from kotlin and kotlin from java
+//with this we can have kotlin and java files both in same project
+//we can call function made in koltin file inside java
+fun main(args:Array<String>) {
+
+}
+fun add(a:Int,b:Int):Int{
+    return a+b
+}
